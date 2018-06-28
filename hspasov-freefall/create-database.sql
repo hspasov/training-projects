40950f41756568d8a40625e003c14920a6968f7e
Hristo Spasov <hristo@arc-global.org>
2018-06-28 19:49:13 +0300

    Update create database. Add fetch-data, in which connecting to api and sqlite db.

--- hspasov-freefall/create-database.sql
+++ hspasov-freefall/create-database.sql
@@ -1,66 +1,75 @@
-CREATE TABLE freefall.data_fetches (
-	id int PRIMARY KEY NOT NULL,
+DROP TABLE IF EXISTS data_fetches;
+DROP TABLE IF EXISTS routes;
+DROP TABLE IF EXISTS airports;
+DROP TABLE IF EXISTS airlines;
+DROP TABLE IF EXISTS flights;
+DROP TABLE IF EXISTS routes_flights;
+DROP TABLE IF EXISTS subscriptions;
+
+CREATE TABLE data_fetches (
+	id integer PRIMARY KEY,
 	timestamp text NOT NULL
 );
 
-CREATE TABLE freefall.subscriptions (
-	id int PRIMARY KEY NOT NULL,
+CREATE TABLE subscriptions (
+	id integer PRIMARY KEY,
 	fly_from text NOT NULL,
 	fly_to text NOT NULL,
-	is_roundtrip int NOT NULL DEFAULT 0,
+	is_roundtrip integer NOT NULL DEFAULT 0,
 	UNIQUE(fly_from, fly_to, is_roundtrip),
 	CHECK(fly_from <> fly_to)
 );
 
-CREATE TABLE freefall.routes (
-	id int PRIMARY KEY NOT NULL,
+CREATE TABLE routes (
+	id integer PRIMARY KEY,
 	booking_token text NOT NULL UNIQUE,
-	subscription_id int NOT NULL,
-	data_fetch_id int NOT NULL,
+	subscription_id integer NOT NULL,
+	data_fetch_id integer NOT NULL,
 	FOREIGN KEY(subscription_id) REFERENCES subscriptions(id),
 	FOREIGN KEY(data_fetch_id) REFERENCES data_fetches(id)
 );
 
-CREATE TABLE freefall.airports (
-	id int PRIMARY KEY NOT NULL,
+CREATE TABLE airports (
+	id integer PRIMARY KEY,
 	iata_code text NOT NULL UNIQUE,
 	name text NOT NULL UNIQUE,
-	data_fetch_id int NOT NULL,
+	data_fetch_id integer NOT NULL,
 	FOREIGN KEY(data_fetch_id) REFERENCES data_fetches(id)
 );
 
-CREATE TABLE freefall.airlines (
-	id int PRIMARY KEY NOT NULL,
+CREATE TABLE airlines (
+	id integer PRIMARY KEY,
 	name text NOT NULL UNIQUE,
 	code text NOT NULL UNIQUE,
 	logo_url text UNIQUE,
-	data_fetch_id int NOT NULL,
+	data_fetch_id integer NOT NULL,
 	FOREIGN KEY(data_fetch_id) REFERENCES data_fetches(id) 
 );
 
-CREATE TABLE freefall.flights (
-	id int PRIMARY KEY NOT NULL,
-	airline_id int NOT NULL,
-	airport_from_id int NOT NULL,
-	airport_to_id int NOT NULL,
+CREATE TABLE flights (
+	id integer PRIMARY KEY,
+	airline_id integer NOT NULL,
+	flight_number text NOT NULL,
+	airport_from_id integer NOT NULL,
+	airport_to_id integer NOT NULL,
 	dtime text NOT NULL,
 	atime text NOT NULL,
-	price int NOT NULL, -- stored as the lowest common unit, e.g. cents, pence, etc.
+	price integer NOT NULL, -- stored as the lowest common unit, e.g. cents, pence, etc.
 	currency text NOT NULL,
 	remote_id text NOT NULL UNIQUE,
-	data_fetch_id int NOT NULL,
+	data_fetch_id integer NOT NULL,
 	FOREIGN KEY (airline_id) REFERENCES airlines(id),
 	FOREIGN KEY (airport_from_id) REFERENCES airports(id),
 	FOREIGN KEY (airport_to_id) REFERENCES airports(id),
 	FOREIGN KEY (data_fetch_id) REFERENCES data_fetches(id)   
 );
 
-CREATE TABLE freefall.routes_flights (
-	id int PRIMARY KEY NOT NULL,
-	route_id int NOT NULL,
-	flight_id int NOT NULL,
-	is_return int NOT NULL,
-	data_fetch_id int NOT NULL,
+CREATE TABLE routes_flights (
+	id integer PRIMARY KEY,
+	route_id integer NOT NULL,
+	flight_id integer NOT NULL,
+	is_return integer NOT NULL,
+	data_fetch_id integer NOT NULL,
 	FOREIGN KEY (route_id) REFERENCES routes(id),
 	FOREIGN KEY (flight_id) REFERENCES flights(id),
 	FOREIGN KEY (data_fetch_id) REFERENCES data_fetches(id)
